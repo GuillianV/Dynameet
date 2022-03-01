d0e2b1fc7c1df9462788e35a0b04dc3bf17c28c8
GuillianV <guillianvibert90@gmail.com>
2022-03-01 13:32:49 +0100

    button correction

--- lib/views/landing.dart
+++ lib/views/landing.dart
@@ -1,7 +1,5 @@
-
 import 'package:dynameet/widgets/button.dart';
 import 'package:flutter/material.dart';
-
 
 class Landing extends StatefulWidget {
   const Landing({Key? key, required this.title}) : super(key: key);
@@ -12,22 +10,42 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-             const Image(image: AssetImage("assets/images/Dynameet-orange.png")),
-             ButtonBasic(text: "Hello", pressed:  (){
-               Navigator.push(context, MaterialPageRoute(builder:(context) => Landing(title: "Ouaii") ));
-             },)
-          ],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton:  Container(
+            alignment: Alignment(1, 1.0),
+          child:SizedBox(
+            child: ButtonBasic(
+              text: "Commencer",
+              pressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Landing(title: "Ouaii")));
+              },
+            ),
+            height: 80,
+          ),
         ),
-      ) 
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Image(
+                  image: AssetImage("assets/images/Dynameet-orange.png")),
+              ButtonBasic(
+                text: "Commencer",
+                pressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => Landing(title: "Ouaii")));
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
