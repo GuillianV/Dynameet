8daf1eb2656a6eea0a579003a999eea4fdb34b82
GuillianV <guillianvibert90@gmail.com>
2022-03-03 12:53:06 +0100

    add entreprise & etudiants

---
+++ lib/models/entreprise.dart
@@ -0,0 +1,61 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/uuid_util.dart';
+
+
+var UUID = const Uuid();
+List<Entreprise> listEntreprise = [];
+
+void GenerateEntreprise(){
+  listEntreprise.add(Entreprise.CreateEntreprise("Webb Company", ["loyal","dynamique"], ["jeune startup","motivation"], ["Dev web","Marketteur"]));
+  listEntreprise.add(Entreprise.CreateEntreprise("RATP", ["drole","sympathique"], ["dicipliné"], ["Réseau"]));
+}
+
+class Entreprise {
+
+  String uuid = "";
+  String nom = "";
+  List<String> valeurs = [];
+  List<String> apropos = [];
+  List<String> missions = [];
+    List<String> matchedEtudiant = [];
+
+
+  Entreprise(String _uuid){
+    uuid = _uuid;
+  }
+ 
+factory Entreprise.CreateEntreprise(String _nom,List<String> _valeurs,List<String> _apropos,List<String> _missions) {
+    Entreprise entreprise = Entreprise(UUID.v4());
+    entreprise.valeurs = _valeurs;
+    entreprise.apropos = _apropos;
+    entreprise.missions = _missions;
+    return entreprise;
+  }
+ 
+  static Entreprise? GetEntreprise(String _uuid){
+    Entreprise entreprise = listEntreprise.firstWhere((element) => element.nom == _uuid);
+    if (entreprise != null) {
+      return entreprise;
+    } else {
+      return null;
+    }
+  }
+
+
+
+  static void AddEntreprisesMatched(String _uuid,String _uuidEtudiant){
+    Entreprise? entreprise = GetEntreprise(_uuid);
+    if(entreprise != null){
+      entreprise.matchedEtudiant.add(_uuidEtudiant);
+    }
+  }
+
+  static List<Entreprise>? GetEntreprisesMatched(String _uuidEtudiant){
+    return listEntreprise.where((element) => element.matchedEtudiant.contains(_uuidEtudiant) == true).toList();
+  }
+
+  static  List<Entreprise>? GetAllEntreprise(){
+    return listEntreprise;
+  }
+}
---
+++ lib/models/etudiant.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:uuid/uuid.dart';
+import 'package:uuid/uuid_util.dart';
+
+
+var UUID = const Uuid();
+List<Etudiant> listEtudiant = [];
+
+void GenerateEtudiant(){
+  listEtudiant.add(Etudiant.CreateEtudiant("Timoté","18","https://previews.123rf.com/images/pixinoo/pixinoo1404/pixinoo140400194/27891780-dynamische-nachwuchsf%C3%BChrungs-zum-aufruf-gestikuliert.jpg","Marketteur compétant","Salut moi c'est Timoté, je recherche une alternance dans le domaine du marketting ! Contactez moi !", ["loyal","dynamique"], ["Marketteur"]));
+  listEtudiant.add(Etudiant.CreateEtudiant("Charlene","22","https://www.finexkap.com/app/uploads/2018/03/Yoanna-developpeuse-front-compressor.png","Développeuse en réseaux","Salut moi c'est Charlene, je recherche une alternance dans le domaine de la cybere sécurité ! Contactez moi vite !", ["loyal","assidue"], ["Développeuse","Architecture réseau"]));
+}
+
+
+class Etudiant {
+
+  String uuid = "";
+  String nom = "";
+  String age = "";
+  String photo = "";
+  String titreDescription = "";
+  String description = "";
+  List<String> softSkills = [];
+  List<String> hardSkills = [];
+  List<String> matchedEtudiant = [];
+
+  Etudiant(String _uuid){
+    uuid = _uuid;
+  }
+ 
+factory Etudiant.CreateEtudiant(String nom,String age,String photo,String titreDescription,String description,List<String> softSkills,List<String> hardSkills) {
+    Etudiant etudiant = Etudiant(UUID.v4());
+    etudiant.nom = nom;
+    etudiant.age = age;
+    etudiant.photo = photo;
+    etudiant.titreDescription = titreDescription;
+    etudiant.description = description;
+    etudiant.softSkills = softSkills;
+    etudiant.hardSkills = hardSkills;
+    return etudiant;
+  }
+ 
+
+
+ static Etudiant? GetEtudiant(String uuid){
+    Etudiant etudiant = listEtudiant.firstWhere((element) => element.uuid == uuid );
+    if (etudiant != null)
+      return etudiant;
+    else 
+      return null;
+  }
+
+
+ static  void AddEtudiantsMatched(String uuid,String uuidEtudiant){
+    Etudiant? etudiant = GetEtudiant(uuid);
+    if(etudiant != null){
+      etudiant.matchedEtudiant.add(uuidEtudiant);
+    }
+  }
+
+ static List<Etudiant>? GetEtudiantsMatched(String uuidEntrprise){
+    return listEtudiant.where((element) => element.matchedEtudiant.contains(uuidEntrprise) == true).toList();
+  }
+
+   static List<Etudiant>? GetAllEtudiant(){
+    return listEtudiant;
+  }
+
+}
